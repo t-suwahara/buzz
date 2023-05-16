2b91933786f35780c650d5e8523b585613785037
t-suwahara <t-suwahara@virtualex.co.jp>
2023-05-16 18:13:41 +0900

    コミット3

--- 1.js
+++ 1.js
@@ -1,11 +1,9 @@
 for(let i =1;i<=100;i++){
     if (i%3===0 && i % 5===0){
-        console.log('FizzBuzz')
+        console.log('FizzBuzz');
     }else if(i % 3 === 0){
         console.log('Fizz');
-    {
     
-    }
         
 
 
